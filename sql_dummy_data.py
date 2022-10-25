c9df08260c489f146fe2012f56354dd9629be347
Shad47486 <reshadbhuiyan@gmail.com>
2022-10-25 18:06:29 -0400

    Cont.

---
+++ sql_dummy_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#importing needed modules
+import dbm
+import pandas as pd 
+import sqlalchemy
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+#.env file login 
+load_dotenv()
+GCP_MYSQL_HOSTNAME = os.getenv("GCP_MYSQL_HOSTNAME")
+GCP_MYSQL_USER = os.getenv("GCP_MYSQL_USERNAME")
+GCP_MYSQL_PASSWORD = os.getenv("GCP_MYSQL_PASSWORD")
+GCP_MYSQL_DATABASE = os.getenv("GCP_MYSQL_DATABASE")
+
+connection_string_gcp = f'mysql+pymysql://{GCP_MYSQL_USER}:{GCP_MYSQL_PASSWORD}@{GCP_MYSQL_HOSTNAME}:3306/{GCP_MYSQL_DATABASE}'
+db_gcp = create_engine(connection_string_gcp)
+
---
+++ sql_table_creation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#importing needed modules aswell as pip installing the needed ones 
+import dbm
+import pandas as pd 
+import sqlalchemy
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+from faker import Faker # https://faker.readthedocs.io/en/master/
+import uuid
+import random
+
+#.env file login 
+load_dotenv()
+GCP_MYSQL_HOSTNAME = os.getenv("GCP_MYSQL_HOSTNAME")
+GCP_MYSQL_USER = os.getenv("GCP_MYSQL_USERNAME")
+GCP_MYSQL_PASSWORD = os.getenv("GCP_MYSQL_PASSWORD")
+GCP_MYSQL_DATABASE = os.getenv("GCP_MYSQL_DATABASE")
+
+connection_string_gcp = f'mysql+pymysql://{GCP_MYSQL_USER}:{GCP_MYSQL_PASSWORD}@{GCP_MYSQL_HOSTNAME}:3306/{GCP_MYSQL_DATABASE}'
+db_gcp = create_engine(connection_string_gcp)
+
+#show databases 
+tableNames_gcp = db_gcp.table_names()
+print(db_gcp.table_names)
+
+# creating tables in patients db for: patients, medications, 
+# treatment_procedures, conditions, and social determinants
+
+table_prod_patients = """
+create table if not exists production_patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    zip_code varchar(255) default null,
+    dob varchar(255) default null,
+    gender varchar(255) default null,
+    contact_mobile varchar(255) default null,
+    contact_home varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+medications_table = 
+
+
